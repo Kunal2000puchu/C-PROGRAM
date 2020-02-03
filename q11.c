943d05d87d455dbec7bd5cabef8b60a2af647102
Kunal2000puchu <60376255+Kunal2000puchu@users.noreply.github.com>
2020-02-03 19:24:20 +0530

    Add files via upload

---
+++ q11.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  kunal2
+//
+//  Created by Kuntal Acharya on 01/02/20.
+//  Copyright © 2020 Kuntal Acharya. All rights reserved.
+//
+
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    char a;
+    printf("enter an character\n");
+    scanf("%c",&a);
+    if(isupper(a))
+    {
+        
+        printf("the %d is in upper case\n",a);
+            
+        }
+        else if(islower(a))
+        {
+            printf("the %d is in lower case\n",a);
+            
+        }
+        else
+        {
+            printf("the %d is not even an alphabhet\n",a);
+            
+        }
+    }
+
