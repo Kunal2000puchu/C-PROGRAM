628c6fd3c47d78893f88cec416dfa2fc22c626b0
Kunal2000puchu <60376255+Kunal2000puchu@users.noreply.github.com>
2020-02-03 19:19:45 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,21 @@
+//Q2//
+//  maximum between two number//
+
+#include <stdio.h>
+
+int main()
+{int a,b;
+    printf("enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+if(a>b)
+{
+    printf("the %d is maximum",a);
+    
+}
+else
+    {
+        printf("the %d is maximum",b);
+    }
+}
+ 
+
