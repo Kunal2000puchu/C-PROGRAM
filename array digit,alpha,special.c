3a3df36e935c66c39c5302e15daebee31f60ceee
Kunal Acharya <60376255+Kunal2000puchu@users.noreply.github.com>
2020-05-26 04:17:15 +0530

    Update array digit,alpha,special.c

--- array digit,alpha,special.c
+++ array digit,alpha,special.c
@@ -1,10 +1,4 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+//**write a pro to find alphabhet,digit,special number in c**//
 
 #include <stdio.h>
 #define MAX 20
