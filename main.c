1a26058aefbcffac9b8573b1503f687dc6a14126
Kunal Acharya <60376255+Kunal2000puchu@users.noreply.github.com>
2020-08-30 02:33:17 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+//** Write a C program to add two numbers using pointers.**//
+
+#include <stdio.h>
+int main()
+{
+   int x,y, *p, *q, sum;
+
+   printf("Enter two integers to add\n");
+   scanf("%d%d", &x, &y);
+
+   p = &x;
+   q = &y;
+
+   sum = *p + *q;
+
+   printf("Sum of the numbers = %d\n", sum);
+
+   return 0;
+}
